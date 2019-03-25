e7d514f454c7ccaf1ceee4fe7179f10862699331
vtretyak <tretyak1c@gmail.com>
2019-03-25 10:20:48 +0200

    add: api method

---
+++ MyLogbook.MVCWebApp/Controllers/api/FacultiesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyLogbook.AppContext;
+using MyLogbook.Entities;
+
+namespace MyLogbook.MVCWebApp.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacultiesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FacultiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Faculties
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Faculty>>> GetFaculties()
+        {
+            return await _context.Faculties.ToListAsync();
+        }
+
+        // GET: api/Faculties/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Faculty>> GetFaculty(Guid id)
+        {
+            var faculty = await _context.Faculties.FindAsync(id);
+
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return faculty;
+        }
+
+        // PUT: api/Faculties/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFaculty(Guid id, Faculty faculty)
+        {
+            if (id != faculty.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(faculty).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FacultyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Faculties
+        [HttpPost]
+        public async Task<ActionResult<Faculty>> PostFaculty(Faculty faculty)
+        {
+            _context.Faculties.Add(faculty);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFaculty", new { id = faculty.Id }, faculty);
+        }
+
+        // DELETE: api/Faculties/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Faculty>> DeleteFaculty(Guid id)
+        {
+            var faculty = await _context.Faculties.FindAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            _context.Faculties.Remove(faculty);
+            await _context.SaveChangesAsync();
+
+            return faculty;
+        }
+
+        private bool FacultyExists(Guid id)
+        {
+            return _context.Faculties.Any(e => e.Id == id);
+        }
+    }
+}
